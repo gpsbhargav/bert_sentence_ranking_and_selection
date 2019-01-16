73c5b6a7c9d11ad6389e5d91a949ec8b9297aa48
G P Shrivatsa Bhargav <csagps@nvidia-dgx.serc.iisc.ernet.in>
2019-01-16 23:40:12 +0530

    Changed dot product to FC for scoring sentences

--- code/hotpot_model.py
+++ code/hotpot_model.py
@@ -14,8 +14,8 @@ class SentenceSelector(nn.Module):
         self.options = options
         self.device = device
         self.bert = BertModel.from_pretrained(options.bert_type, cache_dir=options.bert_archive)
-        self.query_for_sentence_ranking = nn.Parameter(torch.randn(1, options.bert_hidden_size, requires_grad=True))
         self.dropout = nn.Dropout(p=options.dropout, inplace=False)
+        self.fc_1 = nn.Linear(options.bert_hidden_size, 1, bias=True)
         
     
     def construct_binary_mask(self, tensor_in, padding_index = 0):
@@ -39,6 +39,6 @@ class SentenceSelector(nn.Module):
         
         paragraph_rep = self.dropout(paragraph_rep)
         
-        raw_scores = torch.sum(self.query_for_sentence_ranking * paragraph_rep, dim=-1)
+        raw_scores = self.fc_1(paragraph_rep).squeeze(dim=-1)
 
         return raw_scores
